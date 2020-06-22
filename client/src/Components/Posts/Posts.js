fd36fbba3eb549f53d528ec17617ef0ac24481c1
Gabriel Angelo <gabriel.angelsad1@gmail.com>
2020-06-22 04:39:04 -0300

    fishish client

--- client/src/Components/Posts/Posts.js
+++ client/src/Components/Posts/Posts.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import api from '../../services/api'
 import './styles.css';
 
 
 const Posts = () => {
+
+    const [posts, setPosts] = useState([]);
+
+
+    useEffect(() => {
+
+        updatePosts();
+
+    }, [])
+
+    async function updatePosts() {
+      
+
+        const response = await api.get('/api/posts');
+        console.log(response)
+        setPosts(...posts, response.data.posts)
+
+    }
 
     return (
 
@@ -10,15 +29,33 @@ const Posts = () => {
         <div className="container-posts">
 
             <header className="post-header">
-                <nav>
-
+                <nav className="nav-header">
+                    <a href="#" className="nav-header-item">Home</a>
 
                 </nav>
             </header>
             <section className="post-content">
 
+                <ul className="post-list">
+                    {
+                        posts.map(post => (
 
-            <h1>Posts</h1>
+              
+                                <li key={post.id} className="post-list-item">
+                                <span className="post-title">{post.title}</span>
+                                <p className="post-body">{post.body}</p>
+                                
+                                <span className="post-author">{post.user_id}</span>
+                              
+                               </li>
+                            
+
+                        ))
+                    }
+               
+                </ul>
+
+ 
             </section>
         </div>
      
