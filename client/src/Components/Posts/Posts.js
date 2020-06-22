b957e7a6e854b151d97bc0142c46b42fc4d2d7ef
Gabriel Angelo <gabriel.angelsad1@gmail.com>
2020-06-22 02:01:34 -0300

    start make frontend

---
+++ client/src/Components/Posts/Posts.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './styles.css';
+
+
+const Posts = () => {
+
+    return (
+
+        <>
+        <div className="container-posts">
+
+            <header className="post-header">
+                <nav>
+
+
+                </nav>
+            </header>
+            <section className="post-content">
+
+
+            <h1>Posts</h1>
+            </section>
+        </div>
+     
+    </>
+
+    )
+
+}
+
+export default Posts
